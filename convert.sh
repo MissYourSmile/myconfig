08435d8bb036eb54e819fe64f1b60cb493e03084
felix <donghaodong555@qq.com>
2025-10-23 01:09:12 +0800

    Update convert.sh and clang-format.txt

--- convert.sh
+++ convert.sh
@@ -1,6 +1,9 @@
+mkdir -p png_2560x1440
 for f in $(ls *.png)
 do
-    echo "convert ${f} to jpg/${f%.*}.jpg"
-    # ffmpeg -i ${f} -vf "format=rgb24, scale=iw:ih, pad=iw:ih:(ow-iw)/2:(oh-ih)/2:white" -q:v 2 jpg/${f%.*}.jpg
-    magick ${f} -background black -flatten jpg/${f%.*}.jpg
+    echo "convert ${f} to png_2560x1440/${f%.*}.png"
+    magick \( -size 2560x1440 canvas:"#1e1e2e" \) \
+           \( "${f}" -trim +repage -resize 1024x576 \)\
+           -gravity southeast -geometry +0+80 -composite \
+           png_2560x1440/${f%.*}.png
 done
